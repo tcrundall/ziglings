63e2fa527c803773d21fbbc1431356abcae9add1
Timothy Crundall <tim.crundall@gmail.com>
2025-02-17 22:47:45 +0100

    Complete exercises 65 - 98 (skipping broken async 84 - 91)

--- exercises/094_c_math.zig
+++ exercises/094_c_math.zig
@@ -26,7 +26,7 @@ const std = @import("std");
 
 const c = @cImport({
     // What do we need here?
-    ???
+    @cInclude("math.h");
 });
 
 pub fn main() !void {
@@ -35,6 +35,7 @@ pub fn main() !void {
 
     // Here we call the C function 'fmod' to get our normalized angle.
     const result = c.fmod(angle, circle);
+    // const result = angle % circle;
 
     // We use formatters for the desired precision and to truncate the decimal places
     std.debug.print("The normalized angle of {d: >3.1} degrees is {d: >3.1} degrees.\n", .{ angle, result });
